03a117aece0f3bf5db9c098a43fc77d08601fb03
kumargaurav5 <67996444+kumargaurav5@users.noreply.github.com>
2023-08-26 13:05:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ from myProjectApp import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=random.randint(2000, 9000) ,debug=True)
+    app.run(host='0.0.0.0' ,debug=False)
